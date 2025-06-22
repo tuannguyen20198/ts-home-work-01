d6bb384b69e8d391186c07f15deb46d66d578898
matocdo2018 <lynguyenthanhtuan@gmail.com>
2025-06-22 12:57:47 +0700

    updated code

--- challenge-01/01-basic-types.ts
+++ challenge-01/01-basic-types.ts
@@ -36,22 +36,3 @@ const isAvailable: boolean = true || false;
 // // Write your code here:
 const countLength: number = fullname.length;
 console.log(countLength);
-
-//Type Inference
-//Challenge 1
-//Assign a number to a variable with no type annotation, what is the variable type inferred by TypeScript?
-// =>TypeScript will automatically infer the type of the variable age1 as number.
-// // Write your code here:
-let age1 = 27;
-
-// ### Challenge 2
-// Assign a string to a variable with no type annotation, what is the variable type inferred by TypeScript?
-// =>TypeScript will automatically infer the type of the variable myPet as string.
-// // Write your code here:
-let myPet = "dog";
-
-// ### Challenge 3
-// Assign a boolean false to a variable with no type annotation, what is the variable type inferred by TypeScript?
-// =>TypeScript will automatically infer the type of the variable isActive as boolean.
-// // Write your code here:
-let isActive = true;
